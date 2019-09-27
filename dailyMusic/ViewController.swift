dab778f771f8f5cbbf3a15430384dbf5af2e8f78
ChrisLee0727 <x3non0727@gmail.com>
2019-09-26 22:32:50 -0400

    Initial Commit

---
+++ dailyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dailyMusic
+//
+//  Created by Chris Lee on 2019-09-26.
+//  Copyright © 2019 ChrisLee0727. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
